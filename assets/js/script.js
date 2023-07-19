7ca80813ccfef3ffcf1baf8712b39492078df050
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-07-19 01:12:05 -0500

    fixed code issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // api key variable
-// console.log(document);
 var apiKey = "1f6f694ad415ec6809cb1502b2268a83";
 
 // city and state variables 
@@ -80,8 +79,6 @@ function runSearch(url,cityState) {
         .then(function (data) {
             console.log(data);
             const { lat, lon } = data[0];
-            // console.log(lat);
-            // console.log(lon);
             //return lat and long variables for use in the weather API
             var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
             // fetch weather data 
@@ -96,74 +93,62 @@ function runSearch(url,cityState) {
                     var index = 1;
                     //set date and variables for "todays" weather
                     var today = dayjs().format('MM/DD/YYYY');
-                    console.log(today);
                     var listItem0 = list[0];
-                    // console.log(i);
                     // set and declare weather data variables 
                     var main0 = listItem0.main;
                     var wind0 = listItem0.wind;
                     var weather0 = listItem0.weather;
                         var dateText0 = today;
                         $("#tomorrow0" + " li:first-child").text("Date: " + dateText0);
-                        // console.log(dateText);
                         var tempText = main0.temp;
                         $("#tomorrow0" + " li:nth-child(2)").text("Temp: " + tempText);
-                        // console.log(tempText);
                         var windText = wind0.speed;
                         $("#tomorrow0" + " li:nth-child(3)").text("Wind: " + windText);
-                        // console.log(windText);
                         var humText = main0.humidity;
                         $("#tomorrow0" + " li:nth-child(4)").text("Humidity: " + humText);
-                        // console.log(humText);
                         var icon = weather0[0].icon;
-                        // console.log(icon);
                         $("#tomorrow0" + " img").attr("src", "https://openweathermap.org/img/wn/" + icon + ".png");
                         $("#tomorrow0" + " img").removeAttr("hidden");
 
                     // for loop for iterating through the 5 day weather data array - increase iteration by 7 for each pass through to get the next days weather
                     for (var i = 1; i < list.length; i++) {
                         var listItem = list[i];
+
                         // set and declare weather data variables 
                         var main = listItem.main;
                         var wind = listItem.wind;
                         var weather = listItem.weather;
                         var dateText = dayjs(listItem.dt_txt).format('MM/DD/YYYY');
                         var dateTime = dayjs(listItem.dt_txt).format('HH');
-                        console.log(dateTime);
                         var dateTimeMinusThree = dayjs(listItem0.dt_txt).subtract(3, 'hour').format('HH');
-                        console.log(dateTimeMinusThree);
                         var tomorrow = dayjs(today).add(1, 'day').format('MM/DD/YYYY');
-                        console.log(tomorrow);
                         var todayPlusFive = dayjs(today).add(6, 'day').format('MM/DD/YYYY');
-                        console.log(todayPlusFive);
                         if (dateText >= tomorrow && dateText < todayPlusFive && dateTime == dateTimeMinusThree) {
-                       
-                        // console.log(i);
 
                         // create dom elements and assign weather data 
                         var elementId = "#tomorrow" + index;
-                        // console.log(searchCardEl.children)
-                        // console.log(elementId);
+
                         var dateText = dayjs(listItem.dt_txt).format('MM/DD/YYYY');
                         $(elementId + " li:first-child").text("Date: " + dateText);
-                        // console.log(dateText);
+
                         var tempText = main.temp;
                         $(elementId + " li:nth-child(2)").text("Temp: " + tempText);
-                        // console.log(tempText);
+
                         var windText = wind.speed;
                         $(elementId + " li:nth-child(3)").text("Wind: " + windText);
-                        // console.log(windText);
+
                         var humText = main.humidity;
                         $(elementId + " li:nth-child(4)").text("Humidity: " + humText);
-                        // console.log(humText);
+
                         var icon = weather[0].icon;
-                        // console.log(icon);
+
                         $(elementId + " img").attr("src", "https://openweathermap.org/img/wn/" + icon + ".png");
                         $(elementId + " img").removeAttr("hidden");
+                        
                         index++;
                         }
                     }
-                    // console.log(data.list)
+
                     cityNameEL.textContent = cityState;
                 })
         })
@@ -173,7 +158,6 @@ function runSearch(url,cityState) {
 window.addEventListener("load", function (event) {
     // pull search history from local storage 
     var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
-    // console.log(searchHistory);
     //button creation variable
     buttonCreate = "button";
     var si = 0;
@@ -226,7 +210,6 @@ $(document).on("click", ".btn-secondary", function (event) {
     event.preventDefault();
     //captures the name of each card to use in geocode api
     var cityState = $(this).text();
-    // console.log(city);
     // set the geocode API to pull lat and long 
     var geoCodeAPI = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityState + ",usa&limit=1&appid=" + apiKey;
     runSearch(geoCodeAPI,cityState);
